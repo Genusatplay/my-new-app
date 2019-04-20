a5d8c7700ba24d486102c98922b21e542e46b3fb
genusatplay <demon_axp@mail.ru>
2019-04-20 12:22:46 +0300

    react project template

---
+++ ClientApp/src/components/Employee.js
@@ -0,0 +1,19 @@
+﻿import React, { Component } from 'react';
+
+export class Employee extends Component {
+    static displayName = Employee.name;
+
+    render() {
+        return (
+            <div>
+                <code>name, email, birthday, salary</code>
+                <table>
+                    <tr><td>Имя</td><td>значение</td></tr>
+                    <tr><td>Почта</td><td>значение</td></tr>
+                    <tr><td>Дата рождения</td><td>значение</td></tr>
+                    <tr><td>Зарплата</td><td>значение</td></tr>
+                </table>
+            </div>
+        );
+    }
+}
---
+++ ClientApp/src/components/Login.js
@@ -0,0 +1,34 @@
+﻿import React, { Component } from 'react';
+//import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+//import AppBar from 'material-ui/AppBar';
+//import RaisedButton from 'material-ui/RaisedButton';
+//import TextField from 'material-ui/TextField';
+import './Login.css';
+
+export class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+    render() {
+        return (
+            <div>
+                <label for="uname"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="uname" required/>
+
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="upwd" required/>
+
+                <button type="submit">Login</button>
+            </div>
+        );
+    }
+}
+const style = {
+    margin: 15,
+    
+};
+export default Login;
